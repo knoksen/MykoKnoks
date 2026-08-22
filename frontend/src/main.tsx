22f6427099382e3d1e6bdd462ca8dd9c1e8e5c13
knoks <31391420+knoksen@users.noreply.github.com>
2026-08-22 20:09:55 +0200

    Load desktop release badge styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './styles.css'
 import './advanced.css'
 import './temporal.css'
+import './version.css'
 
 const rootElement = document.getElementById('root')
 if (!rootElement) throw new Error('MykoKnoks root element is missing')
